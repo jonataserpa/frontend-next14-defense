f787f29706e6858dcc86673fe3b003863d8dcb6b
Jonata Serpa <jonataserpa@gmail.com>
2023-11-29 10:06:49 -0300

    fix: adjust route

--- components/environment/index.ts
+++ components/environment/index.ts
@@ -14,5 +14,5 @@ export const Environment = {
     /**
      * Url base de consultado dos dados dessa aplicação
      */
-    URL_BASE: "https://nest.jonataserpa.net/services",
+    URL_BASE: "https://nest.jonataserpa.net",
   };
